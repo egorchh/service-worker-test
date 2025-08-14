cf4418aa27f0f03db5e97a8e2353ff658795e5fd
egorchh <egorchh@users.noreply.github.com>
2025-08-14 13:51:35 +0000

    deploy: 11266a9d180f3cbff4e45483074a90a16b78008b

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const VERSION = 'v10'
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('message', event => {
+    if (event.data && event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
